63d8cb38c979aba805c2504e3f3bc9ff6239907d
Andrew Szucs <andrew0szucs@gmail.com>
2018-12-26 23:47:30 +0100

    Add style to Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,15 +4,38 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledBanner = styled.div`
+  color: white;
   height: 75vh;
-  display: flex;
-  flex-direction: column;
+  max-height: 50em;
+  min-height: 30em;
+
   align-items: center;
-  justify-content: center;
+  background-attachment: fixed;
+  background-image: url('https://gatsby-forty.surge.sh/static/banner-22e3335cf4be5dddb7a51870b0e3203f.jpg');
+  background-position: 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-bottom: 0 !important;
+  cursor: default;
+  display: flex;
+  margin-bottom: -3.25em;
+  padding: 6em 0 2em;
+  position: relative;
+  top: -3.25em;
 `
 
 const StyledInner = styled.div`
-  --marginSide: 6rem;
+  margin: 0 auto;
+  max-width: 65em;
+  width: calc(100% - 6em);
+
+  padding: 0 !important;
+  position: relative;
+  transition: opacity 1.5s ease, transform 0.5s ease-out, filter 0.5s ease,
+    -webkit-filter 0.5s ease;
+  z-index: 2;
+
+  /* --marginSide: 6rem;
   margin: 0 var(--marginSide);
   width: calc(70% - var(--marginSide));
   transition: opacity 1.5s ease, transform 0.5s ease-out, filter 0.5s ease;
@@ -21,7 +44,23 @@ const StyledInner = styled.div`
     --marginSide: 3rem;
     margin: 0 var(--marginSide);
     width: calc(100% - var(--marginSide));
-  }
+  } */
+`
+
+const StyledColorOverlay = styled.div`
+  background-color: #242943;
+  content: '';
+  display: block;
+  height: 100%;
+  left: 0;
+  opacity: 0.85;
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  transition: opacity 2.5s ease;
+  transition-delay: 0.75s;
+  width: 100%;
+  z-index: 1;
 `
 
 const StyledTitle = styled.h1`
@@ -29,7 +68,7 @@ const StyledTitle = styled.h1`
 `
 
 const StyledHorizontalLine = styled.div`
-  background-color: #000;
+  background-color: #fff;
   content: '';
   display: block;
   height: 2px;
@@ -57,7 +96,7 @@ const Banner = () => {
   return (
     <StyledBanner>
       <StyledInner>
-        <StyledTitle>Hi, my name is Forty</StyledTitle>
+        <StyledTitle>Hi, my name is András</StyledTitle>
         <StyledHorizontalLine />
         <StyledSubtitleContainer>
           <StyledSubtitle>
@@ -65,12 +104,13 @@ const Banner = () => {
             <br />
             and released under the creative commons.
           </StyledSubtitle>
-          <Button>
+          <Button primary>
             Big
             <StyledButtonIcon icon="arrow-right" />
           </Button>
         </StyledSubtitleContainer>
       </StyledInner>
+      <StyledColorOverlay />
     </StyledBanner>
   )
 }
